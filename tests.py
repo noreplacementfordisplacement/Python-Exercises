8f87eb46edbe568e62d8507ed5dbda1d720b136d
noreplacementfordisplacement <daanhoppener@gmail.com>
2018-02-23 22:49:00 +0100

    updated tests

---
+++ tests.py
@@ -0,0 +1,20 @@
+# file for providing validation test data sources and visualization. Creation date wk 1808.5
+
+# import libraries:
+import numpy as np
+
+#input parameters
+dvar = 10 #variance
+dlen = 10 #length of time-series signal
+damp = 10 #amplitude of time series signal
+
+#change at t = x
+t_change = 10
+
+#generate random signal
+
+noise = np.random.normal(0,1,100)
+
+# 0 is the mean of the normal distribution you are choosing from
+# 1 is the standard deviation of the normal distribution
+# 100 is the number of elements you get in array noise
