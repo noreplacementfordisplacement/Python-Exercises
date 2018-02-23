5449c1fe31c8651c12b30d0d89cc87eb499f9024
noreplacementfordisplacement <daanhoppener@gmail.com>
2018-02-23 23:23:35 +0100

    added gr plot capability

--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@
 
 # import libraries:
 import numpy as np
+from gr import pygr
+
 
 #input parameters
 dvar = 10 #variance
@@ -18,3 +20,5 @@ noise = np.random.normal(0,1,100)
 # 0 is the mean of the normal distribution you are choosing from
 # 1 is the standard deviation of the normal distribution
 # 100 is the number of elements you get in array noise
+
+pygr.plot(list(x**2 for x in range(100)))
