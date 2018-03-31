eaeffff4419fe2c196cc3dfc9acfe78799e89a58
noreplacementfordisplacement <daanhoppener@gmail.com>
2018-03-31 20:16:33 +0200

    added ARIMA practice model

---
+++ 18136_arima_and_dataset.py
@@ -0,0 +1,13 @@
+# 18136_arima_and_dataset.py
+
+# import packages
+
+import warnings
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethrityeight')
+
+#data = sm.datasets.co2.load_pandas()
